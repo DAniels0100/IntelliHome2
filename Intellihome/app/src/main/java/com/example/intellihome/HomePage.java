7e0b253779d8dbf53327cccf3003e1830ac19df4
Daniels0100 <se.alchanto@gmail.com>
2024-10-23 18:59:53 -0600

    Conexion perfil-baseDatos

--- Intellihome/app/src/main/java/com/example/intellihome/HomePage.java
+++ Intellihome/app/src/main/java/com/example/intellihome/HomePage.java
@@ -1,27 +1,26 @@
 package com.example.intellihome;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;  // Asegúrate de importar esta clase
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        // Obtener el color pasado desde la actividad anterior
-        String selectedColor = getIntent().getStringExtra("SELECTED_COLOR");
+        // Encuentra el ImageView de perfil
+        ImageView perfilMenu = findViewById(R.id.perfilmenu);
 
-        // Establecer el color de la franja
-        View colorBand = findViewById(R.id.color_band);
-
-        // Verificar si el color seleccionado no es nulo
-        if (selectedColor != null) {
-            colorBand.setBackgroundColor(Color.parseColor(selectedColor));
-        }
+        perfilMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomePage.this, Perfil.class));
+            }
+        });
     }
 }
