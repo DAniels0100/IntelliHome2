ee355602739631ad1d77427febec475920a1497a
unknown <j.castillo.2@estudiantec.cr>
2024-10-24 06:19:32 -0600

    cambios en historial

--- app/src/main/java/com/example/intellihome/Historial.java
+++ app/src/main/java/com/example/intellihome/Historial.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Historial extends AppCompatActivity {
     @Override
@@ -13,14 +18,38 @@ public class Historial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
 
+        // Encuentra el ViewPager2 en el layout
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        // Crea una instancia del adaptador
+        AdaptadorViewPager adapter = new AdaptadorViewPager(this);
+        // Asigna el adaptador al ViewPager2
+        viewPager.setAdapter(adapter);
+
+        // Encuentra el TabLayout
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        // Configura el TabLayout con el ViewPager2
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position) {
+                            case 0:
+                                tab.setText("Pestaña 1"); // Cambia el texto según tus necesidades
+                                break;
+                            case 1:
+                                tab.setText("Pestaña 2"); // Cambia el texto según tus necesidades
+                                break;
+                        }
+                    }
+                }).attach();
+
         // Encuentra el botón de regreso en el layout
         ImageView btnBack = findViewById(R.id.botonRegresar);
-
         // Asigna el evento onClick al botón de regreso
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Redirige a HomePage
                 Intent intent = new Intent(Historial.this, HomePage.class);
                 startActivity(intent);
                 finish(); // Finaliza la actividad actual
@@ -29,7 +58,6 @@ public class Historial extends AppCompatActivity {
 
         // Encuentra el ImageView de perfil
         ImageView perfilMenu = findViewById(R.id.perfilmenu);
-
         perfilMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +67,6 @@ public class Historial extends AppCompatActivity {
 
         // Encuentra el ImageView de historial
         ImageView historial = findViewById(R.id.historial);
-
         historial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,13 +76,11 @@ public class Historial extends AppCompatActivity {
 
         // Encuentra el ImageView de domotica
         ImageView domotica = findViewById(R.id.domotica);
-
         domotica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Historial.this, Domotica.class));
             }
         });
-
     }
 }
